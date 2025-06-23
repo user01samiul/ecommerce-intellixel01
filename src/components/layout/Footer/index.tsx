a3238f732634787c84bd783f082c106d6d1180fb
Md. Samiul Islam <138280535+user01samiul@users.noreply.github.com>
2025-06-24 04:24:00 +0600

    Update index.tsx

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -102,23 +102,7 @@ const Footer = () => {
 
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
-            <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              Shop.co © Made by{" "}
-              <Link
-                href="https://github.com/mohammadoftadeh"
-                className="text-black font-medium"
-              >
-                Mohammad Oftadeh
-              </Link>
-              {", "}
-              Designed by{" "}
-              <Link
-                href="https://www.figma.com/@hamzauix"
-                className="text-black font-medium"
-              >
-                Hamza Naeem
-              </Link>
-            </p>
+            
             <div className="flex items-center">
               {paymentBadgesData.map((badge, _, arr) => (
                 <span
